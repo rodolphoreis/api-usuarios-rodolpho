3b0e282c4e528f8d3e723db2a81cbe5d16834c7f
marcelldac <dacmarcell@gmail.com>
2024-01-08 19:55:12 -0300

    feat: add post, put, patch and delete methods

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//import express from 'express';
 const express = require('express');
+
 const app = express();
 
 let usuarios = [];
@@ -9,16 +11,71 @@ app.get('/usuarios', (request, response) => {
 });
 
 app.post('/usuarios', (request, response) => {
-  const { nome, email, senha } = request.body;
+  const { nome, email, senha, dataDeAniversario, morada, telefone, stack, sobre } = request.body;
   const id = crypto.randomUUID().toString();
   const payload = {
     id,
     nome,
     email,
     senha,
+    dataDeAniversario,
+    morada,
+    telefone,
+    stack,
+    sobre,
   };
   usuarios.push(payload);
   return response.status(201).json(usuarios);
+});
+
+app.put('/usuarios/:id', (request, response) => {
+  const { id } = request.params;
+  const { nome, email, senha, dataDeAniversario, morada, telefone, stack, sobre } = request.body;
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].id === id) {
+      const payload = {
+        id,
+        nome,
+        email,
+        senha,
+        dataDeAniversario,
+        morada,
+        telefone,
+        stack,
+        sobre,
+      };
+      usuarios[i] = payload;
+      return response.status(200).json(usuarios[i]);
+    }
+    return response.status(404).json({ error: 'Usuário não encontrado' });
+  }
+});
+
+app.patch('/usuarios/:id', (request, response) => {
+  const { id } = request.params;
+  const { nome, email, dataDeAniversario, morada, telefone, stack, sobre } = request.body;
+  const index = usuarios.findIndex((usuarios) => {
+    return usuarios.id === id;
+  });
+
+  usuarios[index].nome = nome || usuarios[index].nome;
+  usuarios[index].email = email || usuarios[index].email;
+  usuarios[index].dataDeAniversario = dataDeAniversario || usuarios[index].dataDeAniversario;
+  usuarios[index].morada = morada || usuarios[index].morada;
+  usuarios[index].telefone = telefone || usuarios[index].telefone;
+  usuarios[index].stack = stack || usuarios[index].stack;
+  usuarios[index].sobre = sobre || usuarios[index].sobre;
+
+  return response.status(200).json(usuarios[index]);
+});
+
+app.delete('/usuarios/:id', (request, response) => {
+  const { id } = request.params;
+  const index = usuarios.findIndex((usuarios) => {
+    return usuarios.id === id;
+  });
+  usuarios.splice(index, 1);
+  return response.sendStatus(204);
 });
 
 app.listen(3000, () => {
